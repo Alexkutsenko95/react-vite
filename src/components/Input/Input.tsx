15301cb58761a0da589acec0622234818e92cf96
Kutsenko <oleksandr.kutsenko@inoxoft.com>
2024-04-07 03:10:22 +0300

    fix interface name

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import { FC, ChangeEvent, ReactElement } from 'react';
 import { InputType } from '@/components/Input/constants.ts';
 import './Input.styles.css';
 
-interface ITodoInputProps {
+interface IInput {
   value: string;
   id?: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -10,7 +10,7 @@ interface ITodoInputProps {
   type: InputType; // Use the enum for type prop
 }
 
-export const Input: FC<ITodoInputProps> = ({ ...props }): ReactElement => (
+export const Input: FC<IInput> = ({ ...props }): ReactElement => (
   <input
     className="todo-input"
     required
