8b2804cdd217eba6acf3a76bd92b9e871a96cb5a
Kutsenko <oleksandr.kutsenko@inoxoft.com>
2024-04-09 20:58:14 +0300

    remove comments

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,14 +1,7 @@
 import { describe, expect, test } from 'vitest';
-// import { render, screen } from '@testing-library/react';
-// import App from '@/App';
 
 describe('App', () => {
   test('demo', () => {
     expect(true).toBe(true);
   });
-
-  // test('Renders the main page', () => {
-  //   render(<App />);
-  //   expect(screen.getByText('Vite + React')).toBeDefined();
-  // });
 });
